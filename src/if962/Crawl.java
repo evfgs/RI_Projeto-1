749a74e4d02d3535250868b5ecfa5a68b55720ab
Emanuel Victor <evfgs@cin.ufpe.br>
2017-10-05 11:14:14 -0300

    Updating Crawler - Robots.txt and heuristic

--- src/if962/Crawl.java
+++ src/if962/Crawl.java
@@ -1,5 +1,7 @@
 package if962;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,72 +14,223 @@ import org.jsoup.select.Elements;
 
 public class Crawl
 {
-    // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
-    private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-    private List<String> links = new LinkedList<String>();
-    private Document htmlDocument;
-    private String[] keywords = {"filme", "dvd", "blu-ray", "produto", "titulo", "diretor", "elenco", "duração", "ação", "aventura", "drama", "animação","comédia",
-    							"terror", "infantil", "policial", "guerra", "lançamento", "ficção"};
-    public int linksSize;
+	// We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
+	private static final String USER_AGENT =
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+	private List<String> links = new LinkedList<String>();
+	private Document htmlDocument;
+	private String[] keywordsAtUrl = {"filme", "filmes", "dvd", "blu-ray", "discos", "trilogia", "blu", "ray", "colecao", "3d", "box"};
+	private List<String> exceptions = new LinkedList<String>();
+	private List<String> regexExceptions = new LinkedList<String>();
+	private int countFile = 0;
+	private String hostname;
+	private String htmlFile;
+	//public int linksSize;
 
 
-    public boolean crawl(String url)
-    {
-        try
-        {
-            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
-            Document htmlDocument = connection.get();
-            this.htmlDocument = htmlDocument;
-            if(connection.response().statusCode() == 200)
-            if(!connection.response().contentType().contains("text/html"))
-            {
-                return false;
-            }
-            Elements linksOnPage = htmlDocument.select("a[href]");
-            linksSize = linksOnPage.size();
-            //System.out.println("Found (" + linksOnPage.size() + ") links");
-            for(Element link : linksOnPage)
-            {
-            	
-                this.links.add(link.absUrl("href"));
-            }
-            return true;
-        }
-        catch(IOException ioe)
-        {
-            
-            return false;
-        }
-    }
+	public boolean crawl(String url, String hostname){
+		this.hostname = hostname;
+		try
+		{
+			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+			Document htmlDocument = connection.get();
+			this.htmlDocument = htmlDocument;
+			this.htmlFile = htmlDocument.toString();
+			if(connection.response().statusCode() == 200)
+				if(!connection.response().contentType().contains("text/html"))
+				{
+					return false;
+				}
+			Elements linksOnPage = htmlDocument.select("a[href]");
+			//linksSize = linksOnPage.size();
+			//System.out.println("Found (" + linksOnPage.size() + ") links");
+			for(Element link : linksOnPage)
+			{
+				String pagina = link.absUrl("href").toLowerCase();
+				if(pagina.contains("filme") || pagina.contains("dvd") || pagina.contains("blu-ray")){
+					this.links.add(link.absUrl("href"));
+				}
+				//this.links.add(link.absUrl("href"));
+				//System.out.println(link.absUrl("href"));
+			}
+			//System.out.println("cabo");
+			return true;
+		}
+		catch(IOException ioe)
+		{
+			return false;
+		}
+	}
 
 
 
-    public boolean searchForWord()
-    {
-    	int pageScore = 0;
-        if(this.htmlDocument == null)
-        {
-            System.out.println("Corpo de html inexistente!");
-            return false;
-        }
-        String bodyText = this.htmlDocument.body().text();
-        for(String keyword: keywords){
-        	if( bodyText.toLowerCase().contains(keyword.toLowerCase())){
-        		pageScore++;
-        	}
-        }
-        if(pageScore >=1 && bodyText.contains("R$")){
-        	return true;
-        }
+	/**
+	public boolean searchForWord()
+	{
+		int pageScore = 0;
+		if(this.htmlDocument == null)
+		{
+			System.out.println("Corpo de html inexistente!");
+			return false;
+		}
+		String bodyText = this.htmlDocument.body().text();
+		for(String keyword: keywords){
+			if( bodyText.toLowerCase().contains(keyword.toLowerCase())){
+				pageScore++;
+			}
+		}
+		if(pageScore >=5 && bodyText.contains("R$")){
+			return true;
+		}
 		return false;
-    }
+	}*/
 
 
-    public List<String> getLinks()
-    {
-        return this.links;
-    }
-    
+	public List<String> getLinks()
+	{
+		return this.links;
+	}
+	
+	public void clearLinks(){
+		this.links = new LinkedList<String>();;
+	}
+
+	public void createExceptions(String url) {
+		try{
+			Connection connection = Jsoup.connect(url + "/robots.txt").userAgent(USER_AGENT);
+			Document htmlDocument = connection.get();
+			this.htmlDocument = htmlDocument;
+
+			System.out.println(url + "/robots.txt");
+
+			String body = htmlDocument.body().text();
+
+			while (body.contains("Disallow: ")) {
+				body = body.substring(body.indexOf("Disallow: ") + 10);
+				if (body.contains(" ")) {
+					String exception = body.substring(0, body.indexOf(" "));
+					body = body.substring(body.indexOf(" "), body.length());
+					this.exceptions.add(exception);
+					//System.out.println(exception);
+				}else {
+					String exception = body;
+					this.exceptions.add(exception);
+				}
+			}
+			createRegex(url);
+
+			System.out.println(regexExceptions);
+		}
+		catch(IOException ioe)
+		{
+			System.out.println(ioe);
+		}
+	}
+
+	public void createRegex(String urlPrefix){
+
+		for(String exception: exceptions) {
+			String regex = "";
+
+			if(exception.charAt(0) == '*') {
+				exception = exception.substring(1, exception.length());
+				if(!exception.contains("*")) {
+					regex = ".*(";
+				}
+			}else {
+				regex = "^(";
+			}
+			if(exception.contains("*")) {
+				while(exception.contains("*")) {
+					regex = regex + ".*(" + exception.substring(0, exception.indexOf("*")) + ")";
+					exception = exception.substring(exception.indexOf("*") + 1, exception.length());
+					if(!exception.contains("*")) {
+						regex = regex + ".*(" + exception + ")";
+						exception = exception.substring(exception.indexOf("*") + 1, exception.length());
+						if(regex.charAt(0) == '^') {
+							regex = regex + ")";
+						}
+					}
+				}
+			}else {
+				regex = regex + exception + ")";
+			}
+			regex = fixRegex(regex);
+			this.regexExceptions.add(regex);
+		}
+	}
+
+	public String fixRegex(String regex) {
+		String fixedRegex = "";
+		for(int i = 0; i < regex.length(); i++) {
+			if((regex.charAt(i) + "").matches("[?]")) {
+				fixedRegex = fixedRegex + "\\" + regex.charAt(i);
+			}else {
+				fixedRegex = fixedRegex + regex.charAt(i);
+			}
+		}
+		return fixedRegex;
+	}
+
+	public boolean checkExceptions(String prefix) {
+		boolean valReturn = true;
+		for(String exception: regexExceptions) {
+			if(prefix.matches(exception)) {
+				valReturn = false;
+			}
+		}
+		return valReturn;
+	}
+
+	public void savePage(String givenUrl) throws IOException {
+		String urlAtHtml = "<!--" + givenUrl + "-->\n"; 
+		
+		try 
+		{
+			File f = new File("C:\\Users\\emanu\\Desktop\\outputs\\heuristica\\saraiva\\" + this.hostname + countFile + ".html");
+			FileOutputStream fop = new FileOutputStream(f);
+			fop.write(urlAtHtml.getBytes());
+			fop.write(this.htmlFile.getBytes());
+			fop.flush();
+			fop.close();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		countFile++;
+	}
+	
+	public boolean checkLastBar(String givenUrl){
+		String aux;
+		List<Integer> indices = new LinkedList<Integer>();
+		for(int i = 0; i < givenUrl.length(); i++){
+			if(givenUrl.charAt(i) == '/'){
+				indices.add(i);
+			}
+		}
+		aux = givenUrl.substring(indices.get(indices.size()-1)+1).toLowerCase();
+		return checkRelevance(aux);
+	}
+	
+	public boolean checkRelevance(String text){
+		for(int i = 0; i < keywordsAtUrl.length; i++){
+			if(text.contains("temporada") || text.contains("season")){
+				return false;
+			}
+			if(text.contains(keywordsAtUrl[i].toLowerCase())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkProductAtUrl(String text){
+		if(text.contains("/p/") || text.contains("/pr")){
+			return true;
+		}
+		return false;
+	}
+
 
 }
--- src/if962/MovieCrawler.java
+++ src/if962/MovieCrawler.java
@@ -1,106 +1,118 @@
 package if962;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
+
 
 public class MovieCrawler
 {
-  private static final int PAGES_TO_CRAWL = 100;
-  private static String BASE_URL = "http://www.extra.com.br/";
-  private Set<String> pagesVisited = new HashSet<String>();
-  private List<String> pagesToVisit = new LinkedList<String>();
-  private List<String> pagesReturned = new LinkedList<String>();
-  private List<String> pathsBlocked = new LinkedList<String>();
- 
+	private static final int PAGES_TO_CRAWL = 1000;
+	private static String BASE_URL = "https://www.saraiva.com.br/filmes";
+	private List<String> pagesVisited = new LinkedList<String>();
+	private List<String> pagesToVisit = new LinkedList<String>();
+	private int pagesSaved = 0;
 
-  public void visit(String url)
-  {
-	  try{
-  		URL conn = new URL(url +  "/robots.txt");
-  		URLConnection connection = conn.openConnection();
-  		BufferedReader br = new BufferedReader(
-                  new InputStreamReader(connection.getInputStream()));
-  		String line = null;
-          while((line = br.readLine()) != null) {
-              if(line.startsWith("Disallow: ")){
-              	pathsBlocked.add(line.substring(10));
-              }
-          }
-          System.out.println(pathsBlocked);
-          
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      while(this.pagesReturned.size() < PAGES_TO_CRAWL)
-      {
-          String currentUrl;
-          Crawl crawling = new Crawl();
-          if(this.pagesToVisit.isEmpty())
-          {
-              currentUrl = url;
-              this.pagesVisited.add(url);
-          }
-          else
-          {
-              currentUrl = this.getNextLink();
-              
-              if(!currentUrl.startsWith(BASE_URL))
-              
-              while(!currentUrl.startsWith(BASE_URL)){
-            	currentUrl = this.getNextLink();  
-              }
-             /* while(!currentUrl.startsWith(BASE_URL + "dvdsebluray/FilmeseSeriados/")){
+
+	public void visit(String url) throws IOException{
+
+		Crawl crawling = new Crawl();
+		URL base = new URL(url);
+		String protocol = base.getProtocol();
+		String host = base.getHost();
+		String hostname = protocol +  "://" + host;
+		crawling.createExceptions(hostname);
+
+		while(this.pagesSaved < PAGES_TO_CRAWL){
+
+			String givenUrl;
+			crawling.clearLinks();
+			if(this.pagesToVisit.isEmpty()){
+				givenUrl = url;
+				this.pagesVisited.add(url);
+			}
+			else{
+				givenUrl = this.getNextLink();
+
+				//StartsWith or contains
+				//Alguns sites nao mantem a estrutura da url, por ex: saraiva.com.br/filmes -> saraiva.com.br/filmes/logan
+				//Alguns sites fazem isso: saraiva.com.br -> saraiva.com.br/logan
+				while(!givenUrl.contains(hostname)){
+					givenUrl = this.getNextLink();  
+				}
+				/* while(!currentUrl.startsWith(BASE_URL + "dvdsebluray/FilmeseSeriados/")){
             	  currentUrl = this.getNextLink();
               }*/
-              while(parseRobots(currentUrl) == true){
-            	  currentUrl = this.getNextLink();
-              }
-          }
-          crawling.crawl(currentUrl);
-          boolean success = crawling.searchForWord();
+			}
+			URL filename = new URL(givenUrl);
+			if(crawling.checkExceptions(filename.getFile())){
+				crawling.crawl(givenUrl, host);
+				System.out.println(givenUrl);
+				this.pagesToVisit.addAll(crawling.getLinks());
+				//heuristica vai ser aqui, se True chama savePage
+				//cria um contador, para ver qtas vezes savePage vai ser chamado
+				//CHAMO AS HEURISTICAS PRA CADA DOMINIO, DENTRO DE UM IF COM VARIOS || SE ALGUM FOR TRUE, SALVA PAGE
+				if(host.equals("www.pontofrio.com.br") || host.equals("www.casasbahia.com.br") || host.equals("www.saraiva.com.br") || host.equals("www.extra.com.br")){
+					if(crawling.checkLastBar(givenUrl)){
+						crawling.savePage(givenUrl);
+						this.pagesSaved++;
+					}
+					
+				}
+				if(host.equals("www.magazineluiza.com.br") || host.equals("www.livrariacultura.com.br") || host.equals("www.walmart.com.br")){
+					if(crawling.checkProductAtUrl(givenUrl) && (crawling.checkRelevance(givenUrl))){
+						crawling.savePage(givenUrl);
+						this.pagesSaved++;
+					}
+				}
+				//crawling.savePage(givenUrl);
+				//loop pagesVisited and save in a file.txt for now
+				//TO-DO create a method to download those urls
+				//saveUrls(givenUrl, host.getHost());
+
+			}
+			/** boolean success = crawling.searchForWord();
           if(success)
           {
-              System.out.println(String.format("\n" + currentUrl + "\nContém palavras chave"));
-              System.out.println("Foram encontrados: " + crawling.linksSize + " links!");
-              pagesReturned.add(currentUrl);
+              System.out.println(String.format("\n" + givenUrl + "\nContém palavras chave"));
+              //System.out.println("Foram encontrados: " + crawling.linksSize + " links!");
+              pagesReturned.add(givenUrl);
               //metodo para pegar esses links que contem palavras chave e salvar em um txt
-          }
-          this.pagesToVisit.addAll(crawling.getLinks());
-      }
-  }
+          }*/
 
-  
-  private boolean parseRobots(String currentUrl){
-	  for(String term: pathsBlocked){
-		  if(currentUrl.contains(term)){
-			  return true;
-		  }
-	  }
-	return false;
+		}
+
+	}
+	/** 
+  private void saveUrls(String givenUrl, String hostname){
+	  try{
+		    PrintWriter writer = new PrintWriter("C:\\Users\\emanu\\workspace\\WebCrawler\\src\\outputs\\" + hostname + ".html", "UTF-8");
+		    writer.println(givenUrl);
+		    writer.close();
+		} catch (IOException e) {
+		   // do something
+		}
   }
-  
-  private String getNextLink()
-  {
-      String nextLink;
-      do
-      {
-          nextLink = this.pagesToVisit.remove(0);
-      } while(this.pagesVisited.contains(nextLink));
-      this.pagesVisited.add(nextLink);
-      return nextLink;
-  }
-  
-  public static void main(String[] args) throws URISyntaxException, IOException
-  {
-      MovieCrawler spider = new MovieCrawler();
-      spider.visit(BASE_URL);
-  }
+	 */
+
+
+	private String getNextLink()
+	{
+		String nextLink;
+		do
+		{
+			nextLink = this.pagesToVisit.remove(0);
+		} while(this.pagesVisited.contains(nextLink));
+		this.pagesVisited.add(nextLink);
+		return nextLink;
+	}
+
+	public static void main(String[] args) throws URISyntaxException, IOException
+	{
+		MovieCrawler spider = new MovieCrawler();
+		spider.visit(BASE_URL); 
+
+	}
 }
